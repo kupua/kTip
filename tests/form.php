c6cb8debaed80372c71433212ad96c908a91f2b1
Ricard Osorio <celtric@gmail.com>
2012-11-01 19:05:01 +0100

    added tests

---
+++ tests/form.php
@@ -0,0 +1,45 @@
+<?php
+
+	if (isset($_POST['identifier']) && $_POST['identifier'] == 'form-1') {
+		if (empty($_POST['text'])) {
+			echo "Please insert your name:<br />";
+		} else {
+			echo "Success!";
+			goto form_2;
+		}
+	}
+
+?>
+<form method="post">
+	<input type="hidden" name="identifier" value="form-1" />
+	<input type="text" name="text" />
+	<input type="submit" value="Form 1" />
+</form>
+
+<?php
+
+	form_2:
+
+	if (isset($_POST['identifier']) && $_POST['identifier'] == 'form-2') {
+		if (empty($_POST['text'])) {
+			echo "Please insert your name:<br />";
+		} else {
+			echo "Success!<br />";
+			goto redirect;
+		}
+	}
+
+?>
+<form method="post">
+	<input type="hidden" name="identifier" value="form-2" />
+	<input type="text" name="text" />
+	<input type="submit" value="Form 2" />
+</form>
+
+<?php
+
+	redirect:
+
+?>
+
+<a href="ajax.html" class="kTip-redirect">redirect</a>
