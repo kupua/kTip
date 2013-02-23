be7ab3efbf35d9b055da19f357db8da47acf13e6
Ricard Osorio <celtric@gmail.com>
2013-02-23 14:21:31 +0100

    improved infinite test

--- tests/infinite.php
+++ tests/infinite.php
@@ -9,13 +9,30 @@ $unique_id = rand(0, 10000);
 <p>Infinite <a href="infinite.php" id="infinite-<?=$unique_id?>">tooltips</a>!</p>
 
 <script>
-	var $infinite = $('#infinite-<?=$unique_id?>');
+	var $infinite = $('#infinite-<?=$unique_id?>'),
+	    kTipInstance = $infinite.parents(':kTip').kTip(),
+	    display = kTipInstance.settings.display,
+	    padding = <?=(int)@$_GET['padding']?> || 200;
 
-	$infinite.kTip({
-		display: $infinite.parents(':kTip').kTip().settings.display,
-		overlay: false,
-		css: {
-			padding: '25px 25px 15px'
-		}
-	});
+	if (display == 'modal') {
+		$infinite.text("modals");
+		$infinite.kTip({
+			display: display,
+			overlay: false,
+			ajax: {
+				data: {
+					padding: padding -5
+				}
+			},
+			css: {
+				padding: padding + 'px ' + padding + 'px ' + (padding - 10) + 'px'
+			}
+		});
+	} else {
+		$infinite.kTip({
+			css: {
+				padding: '25px 25px 15px'
+			}
+		});
+	}
 </script>
